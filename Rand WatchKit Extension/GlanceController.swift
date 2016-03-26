1582b48a9e43ec046a9f1b6112ff41e5c451e362
Morten Kals <m.kals@me.com>
2016-03-26 15:03:21 +0100

    Initial Commit

---
+++ Rand WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  Rand WatchKit Extension
+//
+//  Created by Morten Kals on 26/03/16.
+//  Copyright © 2016 Morten Kals. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
